22ade684afe8e1518e3f56c625bc4443bde2c8e7
hieufpoly077 <hieuvuive.pro5670@gmail.com>
2020-11-02 23:21:34 +0700

    Update config.php

--- system/config.php
+++ system/config.php
@@ -1,5 +1,18 @@
 <?php
-
-
-
+$HOST_DB = "HOST_DB"; 
+$NAME_DB="NAME_DB";
+$USER_DB = "USER_DB";
+$PASS_DB = "PASS_DB";
+$ROOT_URL ="ROOT_URL";
+$ADMIN_URL ="ADMIN_URL";
+$SITE_URL ="SITE_URL";
+$SYSTEM_PATH = "SYSTEM_PATH";
+define ($HOST_DB, "localhost");
+define ($NAME_DB,"testdb");
+define ($USER_DB,"root");
+define ($PASS_DB,"05012001");
+define ($ROOT_URL,"/duan1");
+define ($ADMIN_URL,ROOT_URL."/admin");
+define ($SITE_URL,ROOT_URL."/site");
+define ($SYSTEM_PATH,ROOT_URL."/SYSTEM");
 ?>
